68f7fa68a853dfbdc0bfdf82ded32b4aea816155
Deepak Simha <deepaksimha2003@gmail.com>
2026-05-24 19:32:34 +0530

    chore: remove test setup file

--- frontend/src/test-setup.js
+++
@@ -1,9 +0,0 @@
-/**
- * test-setup.js
- *
- * Global test setup file for Vitest.
- * Imports @testing-library/jest-dom to extend Vitest's expect with
- * DOM-specific matchers such as toBeInTheDocument, toHaveTextContent, etc.
- */
-
-import '@testing-library/jest-dom';
